6d253a3e833dc14a1d3b1d70baef9a246ef070ed
Emanuel Turtula <emanuelturtula@gmail.com>
2019-10-08 22:04:27 -0300

    Agregue documentacion 3

--- TP1/Mediciones/mediciones.m
+++ TP1/Mediciones/mediciones.m
@@ -5,59 +5,48 @@ set(0,'defaulttextinterpreter','latex');
 set(0,'defaultlegendinterpreter','latex');
 
 %%
-vo = readtable('VO.CSV');
-x=table2array(vo(:,4));
-x = x + abs(x(1));
-y=(table2array(vo(:,5))+6.08)/2;  %6.08 por el desplazamiento vertical y /2 por la escala
+
+vo = dlmread('VO.CSV',',',10,0);
+
+x=vo(:,4);
+x = x + abs(x(1)); %desplazo el tiempo negativo a 0
+y = vo(:,5);  
 
 figure
 plot(x*1E6,y)
 grid minor
-xlim([0 80])
-ylim([0 15])
+xlim([10 25])
+ylim([2 14])
 xlabel("Tiempo [$\mu$s]")
 ylabel("Tensi\'on [V]")
 
-Fs = 1.5E6;            % Sampling frequency                    
-T = 1/Fs;             % Sampling period       
-L = length(y);             % Length of signal
-t = (0:L-1)*T;        % Time vector
+print('med_tension.eps', '-depsc', '-tiff');
 
-Y = fft(y);
-Y = abs(Y/L);
-Y = Y(1:L);
-Y(2:end-1) = 2*Y(2:end-1);
-f = (0:Fs)/L;
+
+% figure
+% plot(abs())
+% grid minor
+% xlim([10 25])
+% ylim([2 14])
+% xlabel("Tiempo [$\mu$s]")
+% ylabel("Tensi\'on [V]")
+% 
+% print('med_tension.eps', '-depsc', '-tiff');
+
+%%
+io = dlmread('IO2.CSV',',',10,0);
+
+x= io(:,4);
+x = x + abs(x(1)); %desplazo el tiempo negativo a 0
+y = (io(:,5)-0.149)/10;   
 
 figure
-plot(f*1E-3,20*log(Y)) 
-title('Single-Sided Amplitude Spectrum of X(t)')
-xlabel('f (kHz)')
-ylabel('|P1(f)|')
-
-%%
-% io = readtable('IO.CSV');                     ESTE DA MUCHO MAS FEO QUE IO2
-% x=table2array(io(:,4));                       POR ESO LO COMENTO
-% x = x + abs(x(1));
-% y=(table2array(io(:,5)))/0.05;
-% 
-% figure 
-% plot(x*1E6,y)
-% grid minor
-% xlim([0 80])
-% xlabel("Tiempo [$\mu$s]")
-% ylabel("Corriente [mA]")
-
-%%
-io2 = readtable('IO2.CSV');
-x=table2array(io2(:,4));
-x = x + abs(x(1));
-y=(table2array(io2(:,5))+0.14900)/0.05;
-
-figure 
-plot(x*1E6,y)
+plot(x*1E6,y*1E3)
 grid minor
-xlim([0 20])                        %HASTA 80us le puedo dar
+xlim([10 25])
+% ylim([-10 8])
 xlabel("Tiempo [$\mu$s]")
 ylabel("Corriente [mA]")
+
+print('med_corriente.eps', '-depsc', '-tiff');
 
